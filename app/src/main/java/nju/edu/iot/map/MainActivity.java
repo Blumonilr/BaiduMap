382637441e7b73e88ba0043f3f28d12d79adac21
tubinyan <1169236805@qq.com>
2021-04-15 11:21:03 +0800

    Add Activity

--- app/src/main/java/nju/edu/iot/map/MainActivity.java
+++ app/src/main/java/nju/edu/iot/map/MainActivity.java
@@ -1,131 +1,55 @@
 package nju.edu.iot.map;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
-import com.baidu.location.BDAbstractLocationListener;
-import com.baidu.location.BDLocation;
-import com.baidu.location.BDLocationListener;
-import com.baidu.location.LocationClient;
-import com.baidu.location.LocationClientOption;
-import com.baidu.mapapi.map.BaiduMap;
-import com.baidu.mapapi.map.MapStatus;
-import com.baidu.mapapi.map.MapStatusUpdateFactory;
-import com.baidu.mapapi.map.MapView;
-import com.baidu.mapapi.map.MyLocationData;
-import com.baidu.mapapi.model.LatLng;
 import com.nju.iot.map.R;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.time.Instant;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
-
-    // 定位图层显示方式
-    private MyLocationListener myListener = new MyLocationListener();
-
-    private MapView mMapView = null;
-    private BaiduMap mBaiduMap = null;
-    private LocationClient mLocationClient = null;
-
-    private MyLocationData myLocationData;
+    private EditText networkPathEditText;
+    private Spinner networkTypeSpinner;
+    private Button startButton;
+    private String networkType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+        networkPathEditText = findViewById(R.id.network_path_input);
+        networkTypeSpinner = findViewById(R.id.network_type_spinner);
+        startButton = findViewById(R.id.start_button);
 
-        //获取地图控件引用
-        mMapView = findViewById(R.id.bmapView);
-        mBaiduMap = mMapView.getMap();
-
-        initMap();
-        Toast.makeText(getApplicationContext(), "如果发现定位位置错误，请前往设置主动打开定位权限", Toast.LENGTH_SHORT).show();
-    }
-
-    private void initMap() {
-        //普通地图 ,mBaiduMap是地图控制器对象
-        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
-
-        mBaiduMap.setMyLocationEnabled(true);
-
-        //定位初始化
-        mLocationClient = new LocationClient(getApplicationContext());
-        //注册LocationListener监听器
-        mLocationClient.registerLocationListener(myListener);
-
-        //通过LocationClientOption设置LocationClient相关参数
-        LocationClientOption option = new LocationClientOption();
-        option.setOpenGps(true); // 打开gps
-        option.setNeedDeviceDirect(true);
-        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy); // 高精度定位
-        option.setCoorType("bd09ll"); // 设置坐标类型
-        option.setScanSpan(10000);
-        //设置locationClientOption
-        mLocationClient.setLocOption(option);
-        //开启地图定位图层
-        mLocationClient.start();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
-        mMapView.onResume();
-    }
-    @Override
-    protected void onPause() {
-        super.onPause();
-        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
-        mMapView.onPause();
-    }
-    @Override
-    protected void onDestroy() {
-        mLocationClient.stop();
-        mBaiduMap.setMyLocationEnabled(false);
-        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
-        mMapView.onDestroy();
-        mMapView = null;
-        super.onDestroy();
-    }
-
-    /** 点击重新定位位置
-     * */
-    public void resetLocation(View view) {
-        initMap();
-        Toast.makeText(getApplicationContext(), "如果发现定位位置错误，请前往设置主动打开定位权限", Toast.LENGTH_SHORT).show();
-    }
-
-
-    public class MyLocationListener extends BDAbstractLocationListener {
-        @Override
-        public void onReceiveLocation(BDLocation location) {
-            // MapView 销毁后不在处理新接收的位置
-            if (location == null || mMapView == null) {
-                return;
+        networkTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                networkType = getResources().getStringArray(R.array.network_types)[i];
             }
-            String data = location.getLatitude()+" "+location.getLongitude()+" "+Instant.now();
-            Toast.makeText(getApplicationContext(),data,Toast.LENGTH_SHORT).show();
-            myLocationData = new MyLocationData.Builder()
-                    .accuracy(location.getRadius())// 设置定位数据的精度信息，单位：米
-                    .direction(location.getDirection())// 此处设置开发者获取到的方向信息，顺时针0-360
-                    .latitude(location.getLatitude())
-                    .longitude(location.getLongitude())
-                    .build();
-            mBaiduMap.setMyLocationData(myLocationData);
 
-            LatLng ll = new LatLng(location.getLatitude(), location.getLongitude());
-            MapStatus.Builder builder = new MapStatus.Builder();
-            builder.target(ll).zoom(20.0f);
-            mBaiduMap.animateMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
 
-        }
+            }
+        });
+
+        startButton.setOnClickListener(this);
     }
 
 
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(getApplicationContext(), "start Map!", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, MapActivity.class);
+        String networkPath = networkPathEditText.getText().toString();
+        intent.putExtra("NETWORK_PATH", networkPath);
+        intent.putExtra("NETWORK_TYPE", networkType);
+        startActivity(intent);
+    }
 }
